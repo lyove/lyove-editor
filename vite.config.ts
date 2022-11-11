4c5a9307d03526db794242b17a218c42f6ee55a1
Lyove <lyove@outlook.com>
2022-11-11 16:47:52 +0800

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import path from "path";
+import { defineConfig } from "vite";
+import packageJson from "./package.json";
+
+const { resolve } = path;
+
+const getPackageName = () => {
+  return packageJson.name;
+};
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
+  } catch (err) {
+    throw new Error("Name property in package.json is missing.");
+  }
+};
+
+const fileName = {
+  es: `${getPackageName()}.mjs`,
+  cjs: `${getPackageName()}.cjs`,
+  umd: `${getPackageName()}.umd.js`,
+  iife: `${getPackageName()}.iife.js`,
+};
+
+module.exports = defineConfig({
+  base: "./",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 3000,
+    https: false,
+    open: true
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: getPackageNameCamelCase(),
+      formats: ["es", "cjs", "umd", "iife"],
+      fileName: (format) => fileName[format],
+    },
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output:{
+        assetFileNames: `${getPackageName()}.[ext]`
+      }
+    },
+  },
+});
