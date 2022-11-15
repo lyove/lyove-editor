922b967105492879d2af803ad37fa68d019c900a
Lyove <lyove@outlook.com>
2022-11-15 19:54:05 +0800

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,86 +1,100 @@
 /**
  * Load editor
- * @param {*} Editor
  */
-const loadEditor = (Editor) => {
-  const header = document.getElementById("header");
-  const lang = document.getElementById("lang");
-  const rte = document.getElementById("rte");
-  const clear = document.getElementById("clear");
-  const save = document.getElementById("save");
-  let editor;
-  const toggle = (flag) => {
-    Array.from(header.getElementsByTagName("select")).forEach((item) => (item.disabled = flag));
-    clear.disabled = flag;
-  };
-  const init = () => {
-    editor?.destroy();
-    if (rte) {
-      editor = Editor.create(rte, {
-        lang: lang.value,
-        mode: "max",
-        customPlugins: {
-          audio: {
-            browser: "browser/audio.html",
-          },
-          block: {
-            api: "api/{id}.html",
-            browser: "browser/block.html",
-            css: "css/app.css",
-          },
-          iframe: {
-            browser: "browser/iframe.html",
-          },
-          image: {
-            browser: "browser/image.html",
-          },
-          video: {
-            browser: "browser/video.html",
-          },
-        },
-        onValueChange: (value, event) => {
-          console.log(value, event);
-        },
-      });
-    }
-  };
-  lang.addEventListener("change", init);
-  clear.addEventListener("click", () => {
-    editor.setHtml("");
-    window.scrollTo(0, 0);
-  });
-  save.addEventListener("click", () => {
-    if (rte?.hidden) {
-      editor.save();
-      editor.destroy();
-      save.textContent = "Edit";
-      toggle(true);
-    } else {
-      editor.load();
-      save.textContent = "Save";
-      toggle(false);
-    }
-  });
-  init();
-  save.textContent = rte?.hidden ? "Save" : "Edit";
-};
+import Editor from "../src/editor.js";
+import "../src/editor.less";
 
 /**
- * Dynamic import module
+ * Create element and setting attribute
+ * @param {string} name
+ * @param {object} param
+ * @returns HtmlElement
  */
-const { hostname } = window.location;
-const isIP = (str) => {
-  const reg =
-    /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/g;
-  return reg.test(str);
+const createElement = (name, { attributes = {}, html = "" } = {}) => {
+  const element = document.createElement(name);
+  element.innerHTML = html;
+  Object.entries(attributes).forEach(([key, val]) => val && element.setAttribute(key, `${val}`));
+  return element;
 };
-// development
-if (hostname === "localhost" || hostname === "127.0.0.1" || isIP(hostname)) {
-  import("../src/editor.js").then((module) => {
-    loadEditor(module.default);
+
+document.body.insertBefore(
+  createElement("div", { id: "rich-text-editor" }),
+  document.body.lastChild,
+);
+
+const header = document.getElementById("header");
+const lang = document.getElementById("lang");
+const rte = document.getElementById("rte") || document.getElementById("rich-text-editor");
+const clear = document.getElementById("clear");
+const save = document.getElementById("save");
+
+// Config
+const configs = {
+  lang: lang.value,
+  mode: "max",
+  customPlugins: {
+    audio: {
+      browser: "./doc/browser/audio.html",
+    },
+    block: {
+      api: "./doc/api/{id}.html",
+      browser: "./doc/browser/block.html",
+      css: "./doc/css/app.css",
+    },
+    iframe: {
+      browser: "./doc/browser/iframe.html",
+    },
+    image: {
+      browser: "./doc/browser/image.html",
+    },
+    video: {
+      browser: "./doc/browser/video.html",
+    },
+  },
+  onValueChange: (value, event) => {
+    console.log(value, event);
+  },
+};
+
+// Init
+let editor = Editor.create(rte, configs);
+
+const reLoad = (v) => {
+  editor?.destroy();
+  editor = Editor.create(rte, {
+    ...configs,
+    lang: v,
   });
-  import("../src/editor.less").then(() => {});
-} else {
-  // production
-  loadEditor(window.lyoveEditor);
-}
+};
+
+const toggle = (flag) => {
+  Array.from(header.getElementsByTagName("select")).forEach((item) => (item.disabled = flag));
+  clear.disabled = flag;
+};
+
+// Lang change
+lang.addEventListener("change", (e) => {
+  reLoad(e.target.value);
+});
+
+// Clear
+clear.addEventListener("click", () => {
+  editor.setHtml("");
+  window.scrollTo(0, 0);
+});
+
+// Save
+save.addEventListener("click", () => {
+  if (rte?.hidden) {
+    editor.save();
+    editor.destroy();
+    save.textContent = "Edit";
+    toggle(true);
+  } else {
+    editor.load();
+    save.textContent = "Save";
+    toggle(false);
+  }
+});
+
+save.textContent = rte?.hidden ? "Save" : "Edit";
